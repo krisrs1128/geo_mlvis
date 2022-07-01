1d305a4a276bcb6c05b421742f55cfedd7e65e8b
Bruce Zheng <53232883+zmx-bruce@users.noreply.github.com>
2022-07-01 12:46:37 -0500

    Update train_gpu.py

--- Train_Pred/train_gpu.py
+++ Train_Pred/train_gpu.py
@@ -6,8 +6,8 @@ import pickle
 import shutil
 
 args = {
-    "batch_size": 2, # make this bigger if you are not running on binder
-    "epochs": 30,
+    "batch_size": 5, # make this bigger if you are not running on binder
+    "epochs": 50,
     "lr": 0.0001,
     "device": "cuda", # set to "cuda" if GPU is available
     "train_dir": Path("train_npy"),
